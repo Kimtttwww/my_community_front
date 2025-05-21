cff95054b65b9d2e7b7883206dc56268b8fad6ed
kimtttwww <kimtttwww@gmail.com>
2025-05-21 10:14:25 +0900

    view page 일부 구현

--- src/page/board/BoardViewPage.tsx
+++ src/page/board/BoardViewPage.tsx
@@ -1,4 +1,9 @@
+'use client'
+
+import css from "@/entity/board/css/viewPage.module.css";
+import { Board } from "@/entity/board/type/BoardTypes";
 import { getBoard } from "@/feature/board/api/boardGetApi";
+import { useEffect, useState } from "react";
 
 type ownProps = {
   domain: string,
@@ -6,12 +11,24 @@ type ownProps = {
 };
 
 export default function BoardViewPage({ domain, boardNo }: ownProps) {
+  const [board, setBoard] = useState<Board>();
 
-  getBoard(domain, Number(boardNo)).then((board) => {
-    console.log(board);
-  }).catch((msg) => {
-    console.log(msg);
-  });
+  useEffect(() => {
+    getBoard(domain, Number(boardNo)).then((board) => {
+      setBoard(board);
+    });
+  }, []);
 
-  return (<div></div>);
+  return (<div className="flex" style={{ width: '1000px', minHeight: '600px', flexDirection: 'column', margin: '0 auto', marginTop: '50px' }}>
+    <h1 className={`flex ${css.spacing}`} style={{ textTransform: 'uppercase'/*, borderBottom: 'solid 1px gray'*/ }}>{domain}</h1>
+    <hr className={`${css.spacing}`} style={{ borderColor: 'gray' }} />
+
+    <section className={`flex ${css.spacing}`} style={{flexDirection: 'column', padding: '3px 0'}}>
+      <article><b>{board?.title}</b></article>
+      <article>{board?.writer}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{board?.created}</article>
+    </section>
+    <hr className={`${css.spacing}`} style={{ borderColor: 'lightgray' }} />
+
+    <div style={{paddingTop: '10px'}}>{board?.content}</div>
+  </div>);
 }
